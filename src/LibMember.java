c19d6fec4dafbb7b0f31b500c97692796c3dc683
thunder-bh <yusufyk582006@gmail.com>
2025-11-15 19:26:12 +0300

    Update LibMember.java

--- src/LibMember.java
+++ src/LibMember.java
@@ -1,3 +1,55 @@
 public class LibMember {
-    // Library member implementation goes here
+    
+    private String firstName;
+    private String lastName;
+    private char gender;
+    private long cprNum;
+    private String teleNum;
+    private int numBookIssued;
+    private Book [] booksIssued = new Book[10];
+
+    public LibMember(){
+        firstName="";
+        lastName="";
+        gender='U';
+        cprNum=0;
+        teleNum="";
+        booksIssued = new Book[10];
+        numBookIssued=0;
+    }//defult constracture
+
+    public LibMember(String f, String l, char g, long c, String t){
+        firstName=f;
+        lastName=l;
+        gender=g;
+        cprNum=c;
+        teleNum=t;
+        booksIssued = new Book[10];
+        numBookIssued=0;
+    }//second constractor
+
+    public void setFirstName(String f) { firstName = f; }
+    public void setLastName(String l) { lastName = l; }
+    public void setGender(char g) { gender = g; }
+    public void setCprNum(long cpr) { cprNum = cpr; }
+    public void setTeleNum(String tel) { teleNum = tel; }
+    public void setBooksIssued(Book[] books) { booksIssued = books; }
+    public void setNumBooksIssued(int num) { numBookIssued = num; }
+    //all set methods
+
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public char getGender() { return gender; }
+    public long getCprNum() { return cprNum; }
+    public String getTeleNum() { return teleNum; }
+    public Book[] getBooksIssued() { return booksIssued; }
+    public int getNumBookIssued() { return numBookIssued; }
+    //all get methods
+
+
+
+    public String toString(){
+        String converted= "Name: " + firstName + " " + lastName + "\ngender: " + gender + "\nCPR: " +cprNum + "\nTelephone: " +teleNum + "\nNumber of books issued: " + numBookIssued; 
+        return converted;
+    }
 }
